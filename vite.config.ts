f11ec06602f23ecd8f9702af4d17fb93f3e19e8b
Ezequiel <eolavaca@gmail.com>
2025-03-27 08:38:52 -0300

    testing set up ready

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, // allows us to use vitest library methods in unit test without explicit imports
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts',  // path to setup file
+  },
 })
