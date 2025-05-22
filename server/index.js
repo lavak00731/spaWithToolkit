e2089d3ab4cc8f5b01cc920d3aa07e630b68b43a
Ezequiel <eolavaca@gmail.com>
2025-05-22 08:56:22 -0300

    user creating backend

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ server.post('/api/auth/login', (req, res) => {
 });
 server.post('/api/auth/register', (req, res)=>{
     const {username, name, email, password} = req.body;
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
     const passwordRegex = /^(?=(?:.*[A-Z]){2,})(?=.*\d).{8,}$/;
     const dataTemplate = {
             "id": null,
@@ -36,25 +36,38 @@ server.post('/api/auth/register', (req, res)=>{
         dataTemplate.username = username;
         dataTemplate.name = name;
         
-        //todo check if username and name exist
-        
-        //check if email is wellformed
-        if(emailRegex.test(email)) {
-            dataTemplate.email = email;
-            console.log("issues line 40, email does not match the criteria");
-            return res.sendStatus(400)
-        }
-        //check if password matches the requeriment of 2 capital letters, minimum 8 characters, and at least one number
-        if(passwordRegex.test(password)) {
-            dataTemplate.password = password;
-            console.log("issues line 45, password does not match the criteria");
+        //todo check if username, name, and email exist
+        const isUserCreated = users.map(user => user.username === username && user.email === email );
+        console.log(isUserCreated)
+        if(!isUserCreated[0]) {
+            //check if email is wellformed
+            if(!emailRegex.test(email)) {
+                console.log("issues line 44, email does not match the criteria");
+                return res.sendStatus(400);
+            } 
+            dataTemplate.email = email;              
+            
+            //check if password matches the requeriment of 2 capital letters, minimum 8 characters, and at least one number
+            if(!passwordRegex.test(password)) {
+                console.log("issues line 50, password does not match the criteria");
+                return res.sendStatus(400);
+            }
+            dataTemplate.password = password;            
+            
+            const lastId = Math.max(...users.map(user => user.id))+1;
+            dataTemplate.id = lastId.toString();
+            users.push(dataTemplate);
+            fs.writeFile("./users.json", JSON.stringify(json, null, 2), "utf8", (err)=>{
+                if (err) throw err;
+                console.log('The file has been saved!');
+            });
+            res.status(201).json(dataTemplate); // 201 = created
+        } else {
+            console.log("line 42, user already created");
             return res.sendStatus(400);
         }
-        const lastId = Math.max(...json.users.map(user => user.id));
-        dataTemplate.id = lastId;
-
     } else {
-        console.log("line 45, one of the required data is empty or null")
+        console.log("line 35, one of the required data is empty or null")
         return res.sendStatus(400);
     }
     
